4e54591ec005277679123182a973ab57668d1e5a
Rutwik Sharadrao Khurpade <106751275+rutwik2172000@users.noreply.github.com>
2025-12-23 07:07:03 +0530

    first commit

---
+++ day2/task1_armstrongnumber.js
@@ -0,0 +1,21 @@
+const isArmstrong = (num) => {
+  let sum = 0;
+  let temp = num;
+  let count = num.toString().length;
+
+  while (temp > 0) {
+    let digit = temp % 10;
+    sum += digit ** count;
+    temp = Math.floor(temp / 10);
+  }
+
+  return sum === num;
+};
+
+for (let i = 1; i <= 1000; i++) {
+  if (isArmstrong(i)) {
+    console.log(i);
+  }
+}
+
+
